bff9ffde3d8c62aa4416cebde7817486384be0b7
cindelina <69796142+cindelina@users.noreply.github.com>
2023-04-14 18:42:51 +1000

    updated basket attributes from basket json

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -16,12 +16,11 @@ function isFood(department, category) {
     "HEALTH FOODS",
     "BISCUITS",
     "CONDIMENTS",
+    "CONFECTIONERY"
   ];
 
   let foodDept = departmentList.includes(department);
   let foodCat = categoryList.includes(category);
-
-  console.log(foodDept || foodCat);
 
   return foodDept || foodCat;
 }
@@ -34,31 +33,37 @@ export function processCart(rawBasket) {
   rawBasket.lineItems = [];
 
   rawBasket.AvailableItems.forEach((item) => {
-    if (
-      isFood(
-        item.SapCategories.SapDepartmentName,
-        item.SapCategories.SapCategoryName
-      )
-    ) {
-      // adds food items to food item list?
-      console.log("IS FOOD");
+    if (isFood(item.SapCategories.SapDepartmentName, item.SapCategories.SapCategoryName)){
+      // adds food items to food item list
+      var nutInfo = item.AdditionalAttributes.nutritionalinformation;
+      console.log(nutInfo);
+      console.log(item.Name + " is food");
       foodBasket.lineItems.push(
         new LineItem(
           item.Name,
           item.SalePrice,
           item.SapCategories.SapDepartmentName,
           item.SapCategories.SapCategoryName,
-          item.AdditionalAttributes.allergencontains
+          item.AdditionalAttributes.healthstarrating,
+          item.AdditionalAttributes.allergencontains,
+          item.Quantity,
+          item.LargeImageFile,
+          item.PackageSize,
+          item.AdditionalAttributes.nutritionalinformation
+
         )
       );
     } else {
       // adds non-food tiems to food item list
+      console.log(item.Name + " is not food");
       nonFoodBasket.lineItems.push(
         new LineItem(
           item.Name,
           item.SalePrice,
           item.SapCategories.SapDepartmentName,
-          item.SapCategories.SapCategoryName
+          item.SapCategories.SapCategoryName,
+          item.quantity,
+          item.LargeImageFile
         )
       );
     }
@@ -71,12 +76,18 @@ export function processCart(rawBasket) {
 }
 
 export class LineItem {
-  constructor(name, price, deptName, catName, allergencontains) {
+  constructor(name, price, deptName, catName, starrating, allergencontains, nutInfo, quantity, image, packagesize) {
     this.name = name;
     this.price = price;
     this.deptName = deptName;
     this.catName = catName;
+    this.starrating = starrating;
     this.allergencontains = allergencontains;
+    this.nutInfo = nutInfo;
+    this.quantity = quantity;
+    this.image = image;
+    this.included = true;
+    this.packagesize = packagesize;
   }
 }
 
