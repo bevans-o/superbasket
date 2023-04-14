a892f6321f456cf5f3195982df5eee9ef5eed8a3
Grace <gsendjaja@student.unimelb.edu.au>
2023-04-14 16:46:18 +1000

    categorized food and non-food objects

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,30 +1,83 @@
-export function processCart(rawCart) {
-    console.log('bing bong');
+function isFood(department, category) {
+  const departmentList = [
+    "FRUIT AND VEG",
+    "FRESH CONVENIENT",
+    "PROPRIETARY BAKERY",
+  ];
+  const categoryList = [
+    "VEG",
+    "FRESHCUTS",
+    "HARD PRODUCE",
+    "FRUIT",
+    "MEAT CONVENIENCE",
+    "FROZEN MEALS",
+    "LONGLIFE JUICE",
+    "DRINKS",
+    "HEALTH FOODS",
+    "BISCUITS",
+    "CONDIMENTS",
+  ];
 
-    var cart = {};
-    cart.lineItems = [];
+  let foodDept = departmentList.includes(department);
+  let foodCat = categoryList.includes(category);
 
-    rawCart.AvailableItems.forEach((item) => {
-        cart.lineItems.push(new LineItem(
-            item.Name,
-            item.SalePrice,
-             ))
-    })
+  console.log(foodDept || foodCat);
 
-    console.log(cart);
-
-    return cart;
+  return foodDept || foodCat;
 }
 
+export function processCart(rawBasket) {
+  var nonFoodBasket = {};
+  nonFoodBasket.lineItems = [];
+  var foodBasket = {};
+  foodBasket.lineItems = [];
+  rawBasket.lineItems = [];
 
+  rawBasket.AvailableItems.forEach((item) => {
+    if (
+      isFood(
+        item.SapCategories.SapDepartmentName,
+        item.SapCategories.SapCategoryName
+      )
+    ) {
+      // adds food items to food item list?
+      console.log("IS FOOD");
+      foodBasket.lineItems.push(
+        new LineItem(
+          item.Name,
+          item.SalePrice,
+          item.SapCategories.SapDepartmentName,
+          item.SapCategories.SapCategoryName,
+          item.AdditionalAttributes.allergencontains
+        )
+      );
+    } else {
+      // adds non-food tiems to food item list
+      nonFoodBasket.lineItems.push(
+        new LineItem(
+          item.Name,
+          item.SalePrice,
+          item.SapCategories.SapDepartmentName,
+          item.SapCategories.SapCategoryName
+        )
+      );
+    }
+  });
+
+  console.log(foodBasket.lineItems);
+  console.log(nonFoodBasket.lineItems);
+
+  return [foodBasket, nonFoodBasket];
+}
 
 export class LineItem {
-    constructor(name, price, nutInfo) {
-        this.name = name;
-        this.price = price;
-    }
+  constructor(name, price, deptName, catName, allergencontains) {
+    this.name = name;
+    this.price = price;
+    this.deptName = deptName;
+    this.catName = catName;
+    this.allergencontains = allergencontains;
+  }
 }
 
-export class NutritionalInformation {
-
-}
+export class NutritionalInformation {}
