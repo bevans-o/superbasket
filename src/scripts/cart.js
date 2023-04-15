80b74e55ffbc094e73d11a08eef4754a2c5899bb
Grace <gsendjaja@student.unimelb.edu.au>
2023-04-15 12:57:49 +1000

    added total price

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -4,7 +4,7 @@ function isFood(department, category) {
     "FRUIT AND VEG",
     "FRESH CONVENIENCE",
     "PROPRIETARY BAKERY",
-    "BAKEHOUSE"
+    "BAKEHOUSE",
   ];
   const categoryList = [
     "VEG",
@@ -20,7 +20,7 @@ function isFood(department, category) {
     "CONDIMENTS",
     "CONFECTIONERY",
     "FREEZER - POULTRY",
-    "ICE CREAM"
+    "ICE CREAM",
   ];
 
   let foodDept = departmentList.includes(department);
@@ -30,9 +30,8 @@ function isFood(department, category) {
 }
 
 // pulls the relevant nutritional information from the json file
-function getMacros(nutritionalInformation, quantity){
-
-  if (nutritionalInformation == null){
+function getMacros(nutritionalInformation, quantity) {
+  if (nutritionalInformation == null) {
     return;
   }
 
@@ -43,25 +42,25 @@ function getMacros(nutritionalInformation, quantity){
   const protein = 878;
   const fat = 764;
   const satfat = 253;
-  const sodium = 491; 
+  const sodium = 491;
   const macroOrder = [carbs, kj, satfat, fat, protein, sodium, sugar];
 
-  var returnInfo = []
+  var returnInfo = [];
   var end;
 
-  for(var macro of nutritionalInformation){
-    for (var id of macroOrder){
-      if (macro["Id"] == id){
+  for (var macro of nutritionalInformation) {
+    for (var id of macroOrder) {
+      if (macro["Id"] == id) {
         // to remove the units at the end.
-        if (id == kj || id == sodium){
+        if (id == kj || id == sodium) {
           end = -2;
-        } else{
+        } else {
           end = -1;
         }
-        if(macro["Value"][0] == 'A'){
+        if (macro["Value"][0] == "A") {
           // if the value starts with the substring "APPROX."
           var value = parseFloat(macro["Value"].slice(7, end));
-        } else{
+        } else {
           var value = parseFloat(macro["Value"].slice(0, end));
         }
         returnInfo.push(value);
@@ -69,16 +68,15 @@ function getMacros(nutritionalInformation, quantity){
       }
     }
   }
-  
-  return returnInfo;
 
+  return returnInfo;
 }
 
 // returns a NutritionalInfo which has the sums of each individual macro in the cart
-function basketMacroSum(foodBasket){
-  var sum = new NutritionalInformation([0, 0, 0, 0, 0, 0, 0], 0)
-  for (var food of foodBasket.lineItems){
-    if (food.nutInfo != null){
+function basketMacroSum(foodBasket) {
+  var sum = new NutritionalInformation([0, 0, 0, 0, 0, 0, 0], 0);
+  for (var food of foodBasket.lineItems) {
+    if (food.nutInfo != null) {
       sum.carb += food.nutInfo.carb;
       sum.kj += food.nutInfo.kj;
       sum.satfat += food.nutInfo.satfat;
@@ -87,25 +85,24 @@ function basketMacroSum(foodBasket){
       sum.sodium += food.nutInfo.sodium;
       sum.sugar += food.nutInfo.sugar;
     }
-   }
+  }
 
   return sum;
 }
 
-
 // creates an overall health star rating of the cart
-function overallHealthStar(foodBasket){
+function overallHealthStar(foodBasket) {
   var itemCount = 0;
   var totalStars = 0;
 
-  for (var food of foodBasket.lineItems){
-    if (food.starrating != 0 && food.starrating != null){
-      totalStars += Number(food.quantity)*Number(food.starrating);
+  for (var food of foodBasket.lineItems) {
+    if (food.starrating != 0 && food.starrating != null) {
+      totalStars += Number(food.quantity) * Number(food.starrating);
       itemCount += Number(food.quantity);
     }
   }
 
-  return totalStars/itemCount;
+  return totalStars / itemCount;
 }
 
 export function processCart(rawBasket) {
@@ -116,10 +113,18 @@ export function processCart(rawBasket) {
   //rawBasket.lineItems = [];
 
   rawBasket.AvailableItems.forEach((item) => {
-    if (isFood(item.SapCategories.SapDepartmentName, item.SapCategories.SapCategoryName)){
+    if (
+      isFood(
+        item.SapCategories.SapDepartmentName,
+        item.SapCategories.SapCategoryName
+      )
+    ) {
       // adds food items to food item list
-      
-      var nutInfo = item.AdditionalAttributes.nutritionalinformation === undefined ? null : JSON.parse(item.AdditionalAttributes?.nutritionalinformation);
+
+      var nutInfo =
+        item.AdditionalAttributes.nutritionalinformation === undefined
+          ? null
+          : JSON.parse(item.AdditionalAttributes?.nutritionalinformation);
       console.log(item.Name + " is food");
       foodBasket.lineItems.push(
         new LineItem(
@@ -130,10 +135,14 @@ export function processCart(rawBasket) {
           item.Quantity,
           item.LargeImageFile,
           item.PackageSize,
-          nutInfo === null ? null : new NutritionalInformation(getMacros(nutInfo["Attributes"]), parseFloat(item.Quantity)),
+          nutInfo === null
+            ? null
+            : new NutritionalInformation(
+                getMacros(nutInfo["Attributes"]),
+                parseFloat(item.Quantity)
+              ),
           item.AdditionalAttributes.healthstarrating,
-          item.AdditionalAttributes.allergencontains,
-
+          item.AdditionalAttributes.allergencontains
         )
       );
     } else {
@@ -156,20 +165,35 @@ export function processCart(rawBasket) {
   console.log(nonFoodBasket.lineItems);
   console.log(basketMacroSum(foodBasket));
   console.log("avg health star: " + overallHealthStar(foodBasket));
-  
 
-  return new Basket(foodBasket.lineItems, nonFoodBasket.lineItems);
+  return new Basket(
+    foodBasket.lineItems,
+    nonFoodBasket.lineItems,
+    rawBasket.Totals.SubTotal
+  );
 }
 
 export class Basket {
-  constructor(foodItems, nonFoodItems) {
+  constructor(foodItems, nonFoodItems, totalPrice) {
     this.foodItems = foodItems;
     this.nonFoodItems = nonFoodItems;
+    this.totalPrice = totalPrice;
   }
 }
 
 export class LineItem {
-  constructor(name, price, deptName, catName, quantity, image, packagesize, nutInfo, starrating, allergencontains,) {
+  constructor(
+    name,
+    price,
+    deptName,
+    catName,
+    quantity,
+    image,
+    packagesize,
+    nutInfo,
+    starrating,
+    allergencontains
+  ) {
     this.name = name;
     this.price = price;
     this.deptName = deptName;
@@ -185,14 +209,14 @@ export class LineItem {
 }
 
 export class NutritionalInformation {
-  constructor([carb, kj, satfat, fat, protein, sodium, sugar], quantity){
-    this.carb = carb*quantity; //g
-    this.kj = kj*quantity; //kj
-    this.fat = fat*quantity; //g
-    this.protein = protein*quantity; //g
-    this.satfat = satfat*quantity; //g
-    this.sodium = sodium*quantity; //mg
-    this.sugar = sugar*quantity; //g
+  constructor([carb, kj, satfat, fat, protein, sodium, sugar], quantity) {
+    this.carb = carb * quantity; //g
+    this.kj = kj * quantity; //kj
+    this.fat = fat * quantity; //g
+    this.protein = protein * quantity; //g
+    this.satfat = satfat * quantity; //g
+    this.sodium = sodium * quantity; //mg
+    this.sugar = sugar * quantity; //g
   }
 }
 
